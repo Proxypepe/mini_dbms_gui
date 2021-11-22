32def3795e6dc8ec71dcefd28ffb43206843ebf5
Alex <sasha.golovin.51@bk.ru>
2021-11-22 17:38:33 +0300

    connect to db

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from src.db.repository import Repository
 
 if __name__ == '__main__':
     r = Repository()
+    rows = r.get_column("crude")
+    for row in rows:
+        print(row)
--- src/db/repository.py
+++ src/db/repository.py
@@ -1,7 +1,15 @@
 import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
 from src.core.config import DATABASE_URL
 
 
 class Repository:
     def __init__(self):
-        print(DATABASE_URL)
+        self.engine = create_engine(DATABASE_URL)
+
+    def get_column(self, table_name):
+        with self.engine.connect() as con:
+            rows = con.execute(f"SELECT * FROM {table_name}")
+        return rows
+
